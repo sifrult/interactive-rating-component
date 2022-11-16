bf7cab121c02599391c1d10d68feef732df103f6
Amy Snarr <amy.snarr16@gmail.com>
2022-11-16 12:32:19 -0700

    added simple js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var starEl = $('#star');
+
+var questionEl = $('#feedback-question');
+
+var rateEl = $('#rating-numbers');
+
+var submitBtn = $('#submit-button');
+
+var imageEl = $('#image');
+
+var ratingEl = $('#rating-selection');
+
+var thankYouEl = $('#feedback-thankyou');
+
+submitBtn.hide();
+imageEl.hide();
+ratingEl.hide();
+thankYouEl.hide();
