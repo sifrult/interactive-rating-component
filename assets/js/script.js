2023235f03903ce7583978fbece5ee7d61231dae
Amy Snarr <amy.snarr16@gmail.com>
2022-11-17 12:36:23 -0700

    pressing on a button will change score

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,19 @@ var imageEl = $('#image');
 
 var ratingEl = $('#rating-selection');
 
+var selectedRatingEl = $('#selected-number')
+
 var thankYouEl = $('#feedback-thankyou');
+
+var btn1 = $('.btn1')
+var btn2 = $('.btn2')
+var btn3 = $('.btn3')
+var btn4 = $('.btn4')
+var btn5 = $('.btn5')
 
 imageEl.hide();
 ratingEl.hide();
 thankYouEl.hide();
-
 
 // What happens when you press submit
 function formSubmit(event) {
@@ -32,3 +39,27 @@ function formSubmit(event) {
 
 // Submit rating
 submitBtn.on("click", formSubmit);
+
+btn1.on("click", function() {
+    selectedRatingEl.text("1 ");
+});
+
+
+btn2.on("click", function() {
+    selectedRatingEl.text("2 ");
+});
+
+
+btn3.on("click", function() {
+    selectedRatingEl.text("3 ");
+});
+
+
+btn4.on("click", function() {
+    selectedRatingEl.text("4 ");
+});
+
+
+btn5.on("click", function() {
+    selectedRatingEl.text("5 ");
+});
